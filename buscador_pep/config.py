a1dfe2653c470a0bdd22fceb2ac004fbfe2b662e
Diego A. Pacheco <44652759+DiegoAPacheco@users.noreply.github.com>
2025-02-08 22:12:29 -0300

    Better scraper (#1)
    * Hotfix agrega get_driver en inicialización de Scraper
    
    * Completa y simplifica la configuración
    
    * Hotfix en main

--- buscador_pep/config.py
+++ buscador_pep/config.py
@@ -3,14 +3,23 @@ from pathlib import Path
 from dataclasses import dataclass
 
 @dataclass
+class UserProfile:
+    overview: str
+    location: str
+    industry: str
+    job_opening_status: str
+    job_category: str
+    pay_range: str
+    legal_status: str
+
+@dataclass
 class Configuration:
-    portal_url: str
-    user_profile_path: str
+    portal_url: str = "https://www.empleospublicos.cl/"
+    user_profile: UserProfile
 
 def read_configuration(file_path: Path) -> Configuration:
     with open(file_path, "rb") as f:
         config = tomllib.load(f)
     return Configuration(
-        portal_url = config["portal_url"],
-        user_profile_path = config["user_profile_path"]
+        user_profile = UserProfile(**config["user_profile"])
     )
